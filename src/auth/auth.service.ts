79b535b947fa6e62f7c4e963110b49b39d1e5d08
Michael Bentley <michaelbentley95@gmail.com>
2021-04-28 19:46:36 -0400

    Implement authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,22 @@
 import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/user.entity';
-import { LoginDTO, RegisterDTO } from 'src/models/user.dto';
+import { LoginDTO, RegisterDTO } from 'src/models/user.model';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(UserEntity) private userRepo: Repository<UserEntity>) { }
+    constructor(@InjectRepository(UserEntity) private userRepo: Repository<UserEntity>, private jwtService: JwtService) { }
 
     async register(credentials: RegisterDTO) {
         try {
             const user = this.userRepo.create(credentials);
             await user.save();
-            return user;
+            const payload = { username: user.username };
+            const token = this.jwtService.sign(payload);
+            return { user: { ...user.toJSON, token } };
         }
         catch (err) {
             if (err.code === '23505') {
@@ -27,7 +30,9 @@ export class AuthService {
         try {
             const user = await this.userRepo.findOne({ where: { email: credentials.email } })
             if (user && await user.comparePassword(credentials.password)) {
-                return user;
+                const payload = { username: user.username };
+                const token = this.jwtService.sign(payload);
+                return { user: { ...user.toJSON(), token } };
             }
             throw new Error();
         }
